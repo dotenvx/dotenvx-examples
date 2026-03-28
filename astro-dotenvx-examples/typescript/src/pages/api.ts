bde095d3c34d4de9b807d473cf935fd46761ff5b
Scott Motte <mot@mot.la>
2026-03-28 12:18:59 -0700

    add typescript example

---
+++ astro-dotenvx-examples/typescript/src/pages/api.ts
@@ -0,0 +1,15 @@
+import type { APIRoute } from 'astro';
+
+export const GET: APIRoute = async () => {
+  return new Response(
+    JSON.stringify({
+      ANTHROPIC_API_KEY: process.env.ANTHROPIC_API_KEY ?? null,
+    }),
+    {
+      status: 200,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    }
+  );
+};
