2609ac42abe3649aa3912bc3ea4a88f4c1f023aa
Scott Motte <mot@mot.la>
2026-03-29 09:01:51 -0700

    simplify

--- nextjs-dotenvx-examples/javascript/src/instrumentation.js
+++ nextjs-dotenvx-examples/javascript/src/instrumentation.js
@@ -1,10 +1,4 @@
 export async function register() {
   const { config } = await import('@dotenvx/dotenvx')
-
-  const vercelEnv = process.env.VERCEL_ENV
-  if (vercelEnv === 'production') {
-    config({ path: ['.env.production', '.env'] })
-  } else {
-    config({ path: ['.env'] })
-  }
+  config()
 }
